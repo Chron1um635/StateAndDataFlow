4f7449b882edfb082f43fbaa65796dc8a3c47310
Chron1um635 <chron1um522@gmail.com>
2024-10-12 12:02:52 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Максим Назаров on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
