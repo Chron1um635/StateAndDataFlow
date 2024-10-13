6a0d9a406296c70e5b0c24f45162883955110b09
Chron1um635 <chron1um522@gmail.com>
2024-10-13 13:49:09 +0300

    Setup view models

--- StateAndDataFlow/ContentView/ContentViewViewModel.swift
+++ StateAndDataFlow/ContentView/ContentViewViewModel.swift
@@ -5,4 +5,52 @@
 //  Created by Максим Назаров on 12.10.2024.
 //
 
+import Observation
 import Foundation
+
+@Observable
+final class ContentViewViewModel {
+    
+    var counter = 3
+    var buttonTitle = "Start"
+    
+    private var timer: Timer?
+    
+    func startTimer() {
+        
+        if counter > 0 {
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,
+                target: self,
+                selector: #selector(updateCounter),
+                userInfo: nil,
+                repeats: true
+            )
+        }
+        
+        buttonDidTapped()
+    }
+    
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+            buttonTitle = "Reset"
+        }
+    }
+    
+    private func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func buttonDidTapped() {
+        if buttonTitle == "Reset" {
+            counter = 3
+            buttonTitle = "Start"
+        } else {
+            buttonTitle = "Wait..."
+        }
+    }
+}
--- StateAndDataFlow/LoginView/LoginViewViewModel.swift
+++ StateAndDataFlow/LoginView/LoginViewViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+
+final class LoginViewViewModel: ObservableObject {
+    @Published var user = User()
+    
+    private let storeManager = StorageManager.shared
+    
+    var validLength: Bool {
+        user.name.count >= 3
+    }
+    
+    func logIn() {
+        user.isLoggedIn.toggle()
+        storeManager.save(user: user)
+    }
+    
+    func logOut() {
+        user.name = ""
+        user.isLoggedIn.toggle()
+        storeManager.deleteUser()
+    }
+}
