9757c0644c938805f204e305c7e92a26fc260559
Chron1um635 <chron1um522@gmail.com>
2024-10-13 13:48:36 +0300

    prepare project

---
+++ StateAndDataFlow/LoginView/LoginViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewViewModel.swift
+//  StateAndDataFlow
+//
+//  Created by Максим Назаров on 12.10.2024.
+//
+
+import Foundation
---
+++ StateAndDataFlow/Services/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  StateAndDataFlow
+//
+//  Created by Максим Назаров on 12.10.2024.
+//
+
+import Foundation
