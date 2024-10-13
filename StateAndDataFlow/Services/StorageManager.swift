64fe276a6879d492193b1c902af10a1ddce5ef3f
Chron1um635 <chron1um522@gmail.com>
2024-10-13 13:49:48 +0300

    Setup storage manager

--- StateAndDataFlow/Services/StorageManager.swift
+++ StateAndDataFlow/Services/StorageManager.swift
@@ -5,4 +5,27 @@
 //  Created by Максим Назаров on 12.10.2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    @AppStorage("user") private var userData: Data?
+    
+    private init() {}
+    
+    func fetchUser() -> User {
+        guard let userData else { return User() }
+        let user = try? JSONDecoder().decode(User.self, from: userData)
+        return user ?? User()
+    }
+    
+    func save(user: User) {
+        userData = try? JSONEncoder().encode(user)
+    }
+    
+    func deleteUser() {
+        userData = nil
+    }
+}
